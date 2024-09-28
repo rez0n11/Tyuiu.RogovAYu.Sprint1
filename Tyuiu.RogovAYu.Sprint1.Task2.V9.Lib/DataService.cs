a18e07b2d5130e3e1647ddd267d966819a95d31a
REZ0N11\rezon <rez0n11@ya.ru>
2024-09-28 23:31:07 +0500

    task2

--- Tyuiu.RogovAYu.Sprint1.Task2.V9.Lib/DataService.cs
+++ Tyuiu.RogovAYu.Sprint1.Task2.V9.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.RogovAYu.Sprint1.Task2.V9.Lib
     {
         public double CalculateVolumeCircle(int r)
         {
-            return Math.Round((4.0 / 3 * 3.14 * Math.Pow(r,3)),3);
+            return Math.Round((4.0 / 3 * Math.PI * Math.Pow(r,3)),3);
         }
     }
 }
