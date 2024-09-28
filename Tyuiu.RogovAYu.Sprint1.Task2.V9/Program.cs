dd8dc908b053275dca11d69b0974fdc96d58e182
REZ0N11\rezon <rez0n11@ya.ru>
2024-09-29 00:13:48 +0500

    task2lib

--- Tyuiu.RogovAYu.Sprint1.Task2.V9/Program.cs
+++ Tyuiu.RogovAYu.Sprint1.Task2.V9/Program.cs
@@ -10,29 +10,15 @@ namespace Tyuiu.RogovAYu.Sprint.Task.V
             DataService ds = new DataService();
             double result;
 
-            Console.Title = "Task:1.0.v12| Рогов А.Ю., ПКТб-24-1";
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #12                                                             *");
-            Console.WriteLine("* Выполнил: Рогов Александр Юрьевич |  ПКТб-24-1                          *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Задание:   Вычислить объем сферы, зная радиус (3 знака после запятой)   *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Исходные данные:                                                        *");
-            Console.Write("* Радиус Сферы:");
-            int input = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("***************************************************************************");
 
-            { //code
-                result = ds.CalculateVolumeCircle(input);
-            }
-            Console.WriteLine("* Результат:                                                              *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine($"* Объем Сферы = {result}                                                  *");
-            Console.WriteLine("***************************************************************************");
+            int a = 6, b = 4;
 
+            int x = 30 / 6 + 4;
 
+            a++;
+
+            x += b += a;
+            Console.WriteLine(x);
             Console.ReadKey();
         }
     }
---
+++ Tyuiu.RogovAYu.Sprint1.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.RogovAYu.Sprint1.Task3.V10.Lib
+{
+    public class DataService : ISprint1Task3V10
+    {
+        public double NumberToMoney(double number)
+        {
+            return number;
+        }
+    }
+}
