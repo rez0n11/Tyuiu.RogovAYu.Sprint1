ab6bd0e8254b3102d68d5d56c108a9a1efede0e8
REZ0N11\rezon <rez0n11@ya.ru>
2024-09-29 00:58:07 +0500

    5

--- Tyuiu.RogovAYu.Sprint1.Task5.V3/DataService.cs
+++ Tyuiu.RogovAYu.Sprint1.Task5.V3/DataService.cs
@@ -1,6 +1,6 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 
-namespace Tyuiu.RogovAYu.Sprint1.Task5.V3
+namespace Tyuiu.RogovAYu.Sprint1.Task5.V3.Lib
 {
     public class DataService : ISprint1Task5V3
     {
